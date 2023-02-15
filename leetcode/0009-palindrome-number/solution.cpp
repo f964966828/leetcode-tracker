bf030a11bf8f558b31500fdb25847c19adc947d2
Frank Lin <f964966828@gmail.com>
2023-02-15 16:12:41 +0000

    [LeetCode] - 0009 Easy   - Runtime    19 ms (  5.13%), Memory       6 MB (100.00%) - Palindrome Number

---
+++ leetcode/0009-palindrome-number/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string str1 = to_string(x);
+
+        string str2 = str1;
+        reverse(str2.begin(), str2.end());
+
+        return str1 == str2;
+    }
+};
