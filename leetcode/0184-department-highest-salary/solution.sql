f71982907eeac98ab73cf876ca278babb5a05982
Frank Lin <f964966828@gmail.com>
2023-02-16 14:38:27 +0000

    [LeetCode] - 0184 Medium - Runtime  1233 ms ( 12.08%), Memory       0.0B (100.00%) - Department Highest Salary

---
+++ leetcode/0184-department-highest-salary/solution.sql
@@ -0,0 +1,26 @@
+# Write your MySQL query statement below
+WITH employeeInfo AS(
+    SELECT 
+        d.name as "department",
+        e.name as "employee", 
+        salary
+    FROM employee as e
+    JOIN department as d
+    ON e.departmentId = d.id
+), departmentInfo AS(
+    SELECT
+        department,
+        max(salary) as maxSalary
+    FROM employeeInfo
+    GROUP BY department
+)
+
+SELECT 
+    e.department,
+    employee,
+    salary
+FROM employeeInfo as e
+JOIN  departmentInfo as d
+ON e.department = d.department
+WHERE e.salary = d.maxSalary
+
