3af3dc47cd49d6c7b261e631cd71e587eb9efd36
Frank Lin <f964966828@gmail.com>
2023-02-16 14:22:34 +0000

    [LeetCode] - 0181 Easy   - Runtime   682 ms ( 11.60%), Memory       0.0B (100.00%) - Employees Earning More Than Their Managers

---
+++ leetcode/0181-employees-earning-more-than-their-managers/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT name as Employee
+FROM (
+    SELECT e1.name, e1.salary, e2.salary as manager_salary
+    FROM Employee as e1
+    JOIN Employee as e2
+    ON e1.managerId = e2.id) t
+WHERE salary > manager_salary
