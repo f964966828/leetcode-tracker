b3b2215e88a9bb923c68af98ee863862aaca3b10
Frank Lin <f964966828@gmail.com>
2023-02-18 15:46:49 +0000

    [LeetCode] - 2567 Medium - Runtime   104 ms (  7.44%), Memory    34.2 MB (100.00%) - Minimum Score by Changing Two Elements

---
+++ leetcode/2567-minimum-score-by-changing-two-elements/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimizeSum(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        
+        int n = nums.size();
+        int ans = min(min(nums[n-1]-nums[2], nums[n-3]-nums[0]), nums[n-2]-nums[1]);
+        
+        return ans;
+        
+    }
+};
