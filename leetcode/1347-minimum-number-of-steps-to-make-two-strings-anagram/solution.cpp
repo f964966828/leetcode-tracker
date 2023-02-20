165fe05cfccfafb5ef6131c588bf92b332dfcc05
Frank Lin <f964966828@gmail.com>
2023-02-20 14:02:13 +0000

    [LeetCode] - 1347 Medium - Runtime    54 ms (  6.25%), Memory    16.7 MB ( 99.93%) - Minimum Number of Steps to Make Two Strings Anagram

---
+++ leetcode/1347-minimum-number-of-steps-to-make-two-strings-anagram/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        vector<int> freq1(26), freq2(26);
+        for(char ch: s) freq1[ch-'a']++;
+        for(char ch: t) freq2[ch-'a']++;
+
+        int ans = 0;
+        for(int i=0; i<26; i++) ans += abs(freq1[i] - freq2[i]);
+
+        return ans / 2;
+    }
+};
