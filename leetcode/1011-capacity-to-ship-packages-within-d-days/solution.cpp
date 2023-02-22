fa2e58fcd0f43a3ff4a9058de73d519b2ebc76d2
Frank Lin <f964966828@gmail.com>
2023-02-22 07:44:55 +0000

    [LeetCode] - 1011 Medium - Runtime    55 ms (  5.93%), Memory    26.2 MB (100.00%) - Capacity To Ship Packages Within D Days

---
+++ leetcode/1011-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int l=0, r=0;
+        for(int w: weights){
+            l = max(l, w);
+            r += w;
+        }
+        int ans;
+        while(l<=r){
+            int mid = (l+r)/2, d=0, tmp=0;
+            for(int w: weights){
+                if(tmp+w > mid){
+                    tmp = 0;
+                    d++;
+                }
+                tmp += w;
+            }
+            d++;
+
+            if(d > days)
+                l = mid+1;
+            else{
+                ans = mid;
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
+};
