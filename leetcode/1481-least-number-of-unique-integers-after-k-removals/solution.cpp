8ef34b41560c34c583e1a12fc9433f1921e9d464
Frank Lin <f964966828@gmail.com>
2023-02-27 08:23:15 +0000

    [LeetCode] - 1481 Medium - Runtime   252 ms (  5.05%), Memory    64.2 MB ( 96.17%) - Least Number of Unique Integers after K Removals

---
+++ leetcode/1481-least-number-of-unique-integers-after-k-removals/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        map<int, int> mp;
+        for(int n: arr) mp[n]++;
+        
+        priority_queue <int, vector<int>, greater<int>> pq;
+        for(auto e: mp) pq.push(e.second);
+
+        while(k-pq.top()>=0){
+            k -= pq.top();
+            pq.pop();
+        }
+
+        return pq.size();
+    }
+};
