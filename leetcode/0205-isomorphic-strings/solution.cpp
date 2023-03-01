9116b79d02724f14f32f65aa23fa25bb9ee3de85
Frank Lin <f964966828@gmail.com>
2023-03-01 07:49:38 +0000

    [LeetCode] - 0205 Easy   - Runtime    10 ms (  6.43%), Memory     7.2 MB (100.00%) - Isomorphic Strings

---
+++ leetcode/0205-isomorphic-strings/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char, char> mp1, mp2;
+        for(int i=0; i<s.size(); i++){
+            if(mp1.find(s[i]) == mp1.end()){
+                if(mp2.find(t[i]) != mp2.end()) return false;
+                mp1[s[i]] = t[i];
+                mp2[t[i]] = s[i];
+            }else if(mp1[s[i]] != t[i]) return false;
+        }
+        return true;
+    }
+};
