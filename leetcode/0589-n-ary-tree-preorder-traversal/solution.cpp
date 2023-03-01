16224af4bc1fdcf9409936b118f64ed0b0840ac4
Frank Lin <f964966828@gmail.com>
2023-03-01 08:50:37 +0000

    [LeetCode] - 0589 Easy   - Runtime    34 ms (  5.08%), Memory    16.2 MB ( 12.12%) - N-ary Tree Preorder Traversal

---
+++ leetcode/0589-n-ary-tree-preorder-traversal/solution.cpp
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> ans;
+        if(!root) return ans;
+        
+        ans.push_back(root->val);
+        for(Node* node: root->children)
+            for(int n: preorder(node)) ans.push_back(n);
+
+        return ans;
+    }
+};
