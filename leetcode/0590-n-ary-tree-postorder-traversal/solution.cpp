a914fc515841dd3fc3fd60ac416d22c9f33c3838
Frank Lin <f964966828@gmail.com>
2023-03-06 09:15:59 +0000

    [LeetCode] - 0590 Easy   - Runtime    33 ms (  8.49%), Memory    16.3 MB (  8.15%) - N-ary Tree Postorder Traversal

---
+++ leetcode/0590-n-ary-tree-postorder-traversal/solution.cpp
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        if(!root) return ans;
+        
+        for(Node* node: root->children)
+            for(int n: postorder(node)) ans.push_back(n);
+        ans.push_back(root->val);
+
+        return ans;
+    }
+};
