8cf7599aab60bbab5ede31fe3bc32940022a4dc3
Frank Lin <f964966828@gmail.com>
2023-03-08 19:24:32 +0000

    [LeetCode] - 1527 Easy   - Runtime   797 ms (  5.00%), Memory       0.0B (100.00%) - Patients With a Condition

---
+++ leetcode/1527-patients-with-a-condition/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    patient_id,
+    patient_name,
+    conditions
+FROM patients
+WHERE conditions LIKE '% DIAB1%'
+    OR conditions LIKE 'DIAB1%'
