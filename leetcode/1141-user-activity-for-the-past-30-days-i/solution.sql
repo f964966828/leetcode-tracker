8d7418249aea8a5fe7f5d653834facf6be283722
Frank Lin <f964966828@gmail.com>
2023-03-09 07:37:53 +0000

    [LeetCode] - 1141 Easy   - Runtime   839 ms (  5.62%), Memory       0.0B (100.00%) - User Activity for the Past 30 Days I

---
+++ leetcode/1141-user-activity-for-the-past-30-days-i/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT
+    activity_date as day,
+    count(distinct user_id) as active_users
+FROM activity
+WHERE activity_date between '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date
