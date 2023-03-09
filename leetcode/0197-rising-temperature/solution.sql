1dad0b1e9e3ce08f258d411eaf56973eb0cdd5c9
Frank Lin <f964966828@gmail.com>
2023-03-09 07:31:14 +0000

    [LeetCode] - 0197 Easy   - Runtime   685 ms ( 11.20%), Memory       0.0B (100.00%) - Rising Temperature

---
+++ leetcode/0197-rising-temperature/solution.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+WITH info AS (
+    SELECT
+        w1.id,
+        w1.recordDate,
+        w1.temperature,
+        w2.temperature as temperature2
+    FROM weather w1
+    JOIN weather w2
+    ON DATE_SUB(w1.recordDate, INTERVAL 1 DAY) = w2.recordDate
+)
+
+SELECT 
+    id
+FROM info
+WHERE temperature > temperature2
