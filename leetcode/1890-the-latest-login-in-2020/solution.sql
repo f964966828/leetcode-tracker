eddbccaf77fd1ff3fec7de0fa67e1a079126dace
Frank Lin <f964966828@gmail.com>
2023-03-09 08:44:47 +0000

    [LeetCode] - 1890 Easy   - Runtime  1439 ms (  5.02%), Memory       0.0B (100.00%) - The Latest Login in 2020

---
+++ leetcode/1890-the-latest-login-in-2020/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT
+    user_id,
+    max(time_stamp) as last_stamp
+FROM logins
+WHERE time_stamp BETWEEN '2020-01-01' AND '2021-01-01'
+GROUP BY user_id
