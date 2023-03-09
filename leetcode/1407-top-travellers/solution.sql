229c8a8984576a602d550cf553364d276929fc62
Frank Lin <f964966828@gmail.com>
2023-03-09 08:59:38 +0000

    [LeetCode] - 1407 Easy   - Runtime  1771 ms (  5.02%), Memory       0.0B (100.00%) - Top Travellers

---
+++ leetcode/1407-top-travellers/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    name,
+    IFNULL(SUM(distance), 0) as travelled_distance
+FROM users
+LEFT JOIN rides
+ON users.id = rides.user_id
+GROUP BY users.id, name
+ORDER BY travelled_distance DESC, name
