855b11474cc278ab59420a3e47399f947bebee5a
Frank Lin <f964966828@gmail.com>
2023-03-09 17:45:09 +0000

    [LeetCode] - 0695 Medium - Runtime    25 ms (  5.15%), Memory    24.1 MB (100.00%) - Max Area of Island

---
+++ leetcode/0695-max-area-of-island/solution.cpp
@@ -0,0 +1,37 @@
+typedef vector<vector<int>> vvi;
+
+int dr[] = {-1, 0, 1, 0};
+int dc[] = {0, 1, 0, -1};
+
+class Solution {
+public:
+    int dfs(int r, int c, vvi& visited, vvi& grid){
+        int n = grid.size(), m = grid[0].size();
+
+        int area = 1;
+        visited[r][c] = 1;
+        for(int d=0; d<4; d++){
+            int nr = r+dr[d], nc = c+dc[d];
+            if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+            if(visited[nr][nc] || !grid[nr][nc]) continue;
+            area += dfs(nr, nc, visited, grid);
+        }
+
+        return area;
+    }
+
+    int maxAreaOfIsland(vvi& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vvi visited(n, vector<int>(m));
+        
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!visited[i][j] && grid[i][j])
+                    ans = max(ans, dfs(i, j, visited, grid));
+            }
+        }
+
+        return ans;
+    }
+};
