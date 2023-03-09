75438bd7e1123fbadbb9c2c1fb55e87b88203c1c
Frank Lin <f964966828@gmail.com>
2023-03-09 08:54:09 +0000

    [LeetCode] - 1158 Medium - Runtime  2378 ms (  5.01%), Memory       0.0B (100.00%) - Market Analysis I

---
+++ leetcode/1158-market-analysis-i/solution.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+WITH order_info AS (
+    SELECT
+        buyer_id,
+        COUNT(*) as orders_in_2019
+    FROM orders
+    WHERE order_date BETWEEN '2019-01-01' AND '2020-01-01'
+    GROUP BY buyer_id
+)
+
+SELECT
+    user_id as buyer_id,
+    join_date,
+    IFNULL(orders_in_2019, 0) as orders_in_2019
+FROM users
+LEFT JOIN order_info
+ON user_id = buyer_id
+
