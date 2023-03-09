d3a6a15092443bea3fb7dafb530ed4d6c3a6af07
Frank Lin <f964966828@gmail.com>
2023-03-09 08:37:22 +0000

    [LeetCode] - 1587 Easy   - Runtime  2064 ms (  5.01%), Memory       0.0B (100.00%) - Bank Account Summary II

---
+++ leetcode/1587-bank-account-summary-ii/solution.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+WITH info AS (
+    SELECT
+        account,
+        SUM(amount) as balance
+    FROM transactions
+    GROUP BY account
+)
+
+SELECT
+    users.name,
+    info.balance
+FROM info
+JOIN users
+ON info.account = users.account
+WHERE balance > 10000
