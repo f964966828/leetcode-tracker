5bc4c0ca86bd888e500038986a989d461c9ce454
Frank Lin <f964966828@gmail.com>
2023-03-09 08:32:49 +0000

    [LeetCode] - 1084 Easy   - Runtime  2837 ms (  5.01%), Memory       0.0B (100.00%) - Sales Analysis III

---
+++ leetcode/1084-sales-analysis-iii/solution.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+WITH inside_product AS(
+    SELECT 
+        distinct product_id 
+    FROM sales
+    WHERE sale_date BETWEEN '2019-01-01' AND '2019-03-31'
+), outside_product AS (
+    SELECT
+        distinct product_id
+    FROM sales
+    WHERE sale_date < '2019-01-01' 
+        OR sale_date > '2019-03-31'
+)
+
+SELECT 
+    product_id,
+    product_name
+FROM product
+WHERE product_id IN (SELECT * FROM inside_product)
+    AND product_id NOT IN (SELECT * FROM outside_product)
