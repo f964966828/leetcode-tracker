06b70e7043ee9791481f3a658a0f806084a2a143
Frank Lin <f964966828@gmail.com>
2023-03-10 07:21:58 +0000

    [LeetCode] - 1232 Easy   - Runtime    17 ms (  1.31%), Memory    11.2 MB (100.00%) - Check If It Is a Straight Line

---
+++ leetcode/1232-check-if-it-is-a-straight-line/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        vector<int> xdiff, ydiff;
+        for(int i=1; i<coordinates.size(); i++){
+            vector<int> p1 = coordinates[i-1];
+            vector<int> p2 = coordinates[i];
+            int x1 = p1[0], x2 = p2[0];
+            int y1 = p1[1], y2 = p2[1];
+            xdiff.push_back(x2 - x1);
+            ydiff.push_back(y2 - y1);
+        }
+
+        for(int i=1; i<xdiff.size(); i++){
+            int dx1 = xdiff[i-1], dx2 = xdiff[i];
+            int dy1 = ydiff[i-1], dy2 = ydiff[i];
+            if(dx2*dy1 != dx1*dy2)
+                return false;
+        }
+
+        return true;
+    }
+};
