cebbdc1e0b070f7d2babe045236666d6df50cb06
Frank Lin <f964966828@gmail.com>
2023-03-10 18:31:17 +0000

    [LeetCode] - 0189 Medium - Runtime    28 ms (  5.20%), Memory      25 MB (100.00%) - Rotate Array

---
+++ leetcode/0189-rotate-array/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int temp = k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin()+temp,nums.end());
+        reverse(nums.begin(),nums.begin()+temp);
+    }
+};
