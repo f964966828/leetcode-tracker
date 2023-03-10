b17fd260152ffebc0317db71479f22f3c38f908d
Frank Lin <f964966828@gmail.com>
2023-03-10 18:32:58 +0000

    [LeetCode] - 0167 Medium - Runtime    11 ms (  1.49%), Memory    15.7 MB (100.00%) - Two Sum II - Input Array Is Sorted

---
+++ leetcode/0167-two-sum-ii---input-array-is-sorted/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n=numbers.size(), l=0, r=n-1;
+        while(numbers[l] + numbers[r] != target){
+            if(numbers[l] + numbers[r] < target)
+                l++;
+            else
+                r--;
+        }
+
+        return {l+1, r+1};
+    }
+};
