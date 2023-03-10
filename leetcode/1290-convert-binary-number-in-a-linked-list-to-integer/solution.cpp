e6a9a1de5c6388bcf35a85ba23867c4f07680988
Frank Lin <f964966828@gmail.com>
2023-03-10 11:18:47 +0000

    [LeetCode] - 1290 Easy   - Runtime     0 ms (100.00%), Memory     8.4 MB (100.00%) - Convert Binary Number in a Linked List to Integer

---
+++ leetcode/1290-convert-binary-number-in-a-linked-list-to-integer/solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        vector<int> vec;
+        while(head){
+            vec.push_back(head->val);
+            head = head->next;
+        }
+
+        reverse(vec.begin(), vec.end());
+
+        int mul = 1, sum = 0;
+        for(int n: vec){
+            sum += mul * n;
+            mul *= 2;
+        }
+        
+        return sum;
+    }
+};
