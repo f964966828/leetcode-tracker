42142e09b44fd7970fc108c2d7a9d4a8f1c5b099
Frank Lin <f964966828@gmail.com>
2023-03-11 04:13:58 +0000

    [LeetCode] - 1603 Easy   - Runtime    61 ms (  5.59%), Memory    33.2 MB (100.00%) - Design Parking System

---
+++ leetcode/1603-design-parking-system/solution.cpp
@@ -0,0 +1,25 @@
+class ParkingSystem {
+public:
+    int count[3];
+
+    ParkingSystem(int big, int medium, int small) {
+        count[0] = big;
+        count[1] = medium;
+        count[2] = small;
+    }
+    
+    bool addCar(int carType) {
+        if(count[carType-1]){
+            count[carType-1]--;
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
