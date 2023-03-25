eb5f263b984921a9c31923fa2aaf21515d3259fc
Frank Lin <f964966828@gmail.com>
2023-03-25 08:40:17 +0000

    [LeetCode] - 0020 Easy   - Runtime     0 ms (100.00%), Memory     6.3 MB (100.00%) - Valid Parentheses

---
+++ leetcode/0020-valid-parentheses/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char ch: s){
+            if(ch == '('){
+                st.push(ch);
+            }else if(ch == '['){
+                st.push(ch);
+            }else if(ch == '{'){
+                st.push(ch);
+            }else if(ch == ')'){
+                if(st.empty() || st.top() != '(')
+                    return false;
+                else
+                    st.pop();
+            }else if(ch == ']'){
+                if(st.empty() || st.top() != '[')
+                    return false;
+                else
+                    st.pop();
+            }else if(ch == '}'){
+                if(st.empty() || st.top() != '{')
+                    return false;
+                else
+                    st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
