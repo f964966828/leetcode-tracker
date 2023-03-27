972373d7bbc5f1fe1d44bb85c9ab7f6b594771b7
Frank Lin <f964966828@gmail.com>
2023-03-27 12:26:19 +0000

    [LeetCode] - 0064 Medium - Runtime     8 ms (  5.26%), Memory    10.2 MB (100.00%) - Minimum Path Sum

---
+++ leetcode/0064-minimum-path-sum/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 1e9));
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(i == 1 && j == 1) dp[i][j] = grid[i-1][j-1];
+                else dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i-1][j-1];
+            }
+        }
+
+        return dp[n][m];
+    }
+};
