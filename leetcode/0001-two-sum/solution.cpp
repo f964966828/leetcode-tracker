77f87436a5c980aac8a42b1b0f066e6792e1ec44
Frank Lin <f964966828@gmail.com>
2024-01-18 12:28:35 +0000

    [LeetCode] - 0001 Easy   - Runtime    13 ms ( 38.24%), Memory    10.7 MB (100.00%) - Two Sum

--- leetcode/0001-two-sum/solution.cpp
+++ leetcode/0001-two-sum/solution.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
+        vector<int> diff(nums.size());
+        vector<int> ans(2);
+        int mid = 0;
         for(int i=0; i<nums.size(); i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i] + nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
+            diff[i] = target - nums[i];
+            if(nums[i] == target/2) mid++;
+        }
+        sort(diff.begin(), diff.end());
+
+        for(int i=0; i<nums.size(); i++){
+            auto lower = lower_bound(diff.begin(), diff.end(), nums[i]);
+            
+            if(lower != diff.end() && nums[i] == *lower){
+                if(nums[i] != target / 2 || (nums[i] == target / 2 && mid != 1)){
+                    ans[0] = i;
                 }
+                //cout << nums[ans[0]] << endl;
             }
         }
+        for(int i=0; i<nums.size(); i++){
+            if(target - nums[ans[0]] == nums[i] && ans[0] != i) ans[1] = i; 
+        }
+        
         return ans;
     }
 };
