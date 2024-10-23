6022511f96892d1ede244b2df7dff1681a22aa4a
Frank Lin <f964966828@gmail.com>
2024-10-23 04:54:44 +0000

    [LeetCode] - 2641 Medium - Runtime   157 ms ( 16.02%), Memory   392.9 MB (  8.35%) - Cousins in Binary Tree II

--- leetcode/2641-cousins-in-binary-tree-ii/solution.cpp
+++ leetcode/2641-cousins-in-binary-tree-ii/solution.cpp
@@ -11,49 +11,39 @@
  */
 class Solution {
 public:
+    TreeNode* copy(TreeNode* node, int level, vector<int>& level_sums) {
+        if (!node) return NULL;
+        TreeNode* new_node = new TreeNode(
+            level_sums[level],
+            copy(node->left, level + 1, level_sums),
+            copy(node->right, level + 1, level_sums)
+        );
+        
+        int sum = 0;
+        if (node->left) sum += node->left->val;
+        if (node->right) sum += node->right->val;
+        if (new_node->left) new_node->left->val -= sum;
+        if (new_node->right) new_node->right->val -= sum;
+        if (level == 0) new_node->val = 0;
+
+        return new_node;
+    }
+
     TreeNode* replaceValueInTree(TreeNode* root) {
-        vector<vector<TreeNode*>> treeLayerList;
-        vector<TreeNode*> treeLayer(1, root);
-        
-        while(!treeLayer.empty()){
-            vector<TreeNode*> newLayer;
-            for(auto node: treeLayer){
-                if(!node) continue;
-                newLayer.push_back(node->left);
-                newLayer.push_back(node->right);
+        vector<int> level_sums;
+        vector<TreeNode*> bfs(1, root);
+        while (!bfs.empty()) {
+            int level_sum = 0;
+            vector<TreeNode*> new_bfs;
+            for (auto node : bfs) {
+                level_sum += node->val;
+                if (node->left) new_bfs.push_back(node->left);
+                if (node->right) new_bfs.push_back(node->right);
             }
-            treeLayerList.push_back(treeLayer);
-            treeLayer = newLayer;
+            level_sums.push_back(level_sum);
+            bfs = new_bfs;
         }
-        
-        for(auto layer: treeLayerList){
-            if(layer.size() == 1){
-                layer[0]->val = 0;
-                continue;
-            }
-            
-            int sum = 0;
-            vector<int> valueList;
-            for(auto node: layer){
-                if(node){
-                    valueList.push_back(node->val);
-                    sum += node->val;
-                }else{
-                    valueList.push_back(0);
-                }
-            }
-            
-            for(int i=0; i<valueList.size(); i+=2){
-                valueList[i] = valueList[i+1] = valueList[i] + valueList[i+1];
-            }
-            
-            for(int i=0; i<layer.size(); i++){
-                if(layer[i]){
-                    layer[i]->val = sum - valueList[i];
-                }
-            }
-        }
-        
-        return root;
+
+        return copy(root, 0, level_sums);
     }
 };
