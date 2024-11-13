f3e7d67c587490332f0628abf5ceedd13c8cb4f9
Frank Lin <f964966828@gmail.com>
2024-11-13 07:33:36 +0000

    [LeetCode] - 2563 Medium - Runtime  2336 ms (  5.04%), Memory    60.5 MB (100.00%) - Count the Number of Fair Pairs

--- leetcode/2563-count-the-number-of-fair-pairs/solution.cpp
+++ leetcode/2563-count-the-number-of-fair-pairs/solution.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        
         sort(nums.begin(), nums.end());
         
         long long ans = 0;
-        for(int i=0; i<nums.size(); i++){
-            auto lit = lower_bound(nums.begin()+i+1, nums.end(), lower-nums[i]);
-            auto uit = upper_bound(nums.begin()+i+1, nums.end(), upper-nums[i]);
-            ans += (uit - lit);
+        for (int i = 1; i < nums.size(); i++) {
+            int target_low = lower - nums[i];
+            int target_high = upper - nums[i];
+
+            if (target_high < nums[0]) continue;
+            
+            auto iter_low = lower_bound(nums.begin(), nums.end(), target_low);
+            auto iter_high = --upper_bound(nums.begin(), nums.end(), target_high);
+            
+            int index_low = iter_low - nums.begin();
+            int index_high = min((int)(iter_high - nums.begin()), i - 1);
+            cout << nums[i] << " " << index_low << " " << index_high << endl;
+            ans += max((index_high - index_low + 1), 0);
         }
-        
+
         return ans;
     }
 };
