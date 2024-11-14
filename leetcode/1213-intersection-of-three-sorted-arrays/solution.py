7846cb8ee97b973f0f44546fd2aaf847715c38e0
Frank Lin <f964966828@gmail.com>
2024-11-14 08:13:31 +0000

    [LeetCode] - 1213 Easy   - Runtime     4 ms ( 41.61%), Memory    16.9 MB (100.00%) - Intersection of Three Sorted Arrays

---
+++ leetcode/1213-intersection-of-three-sorted-arrays/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
+        arr = list(set(arr1).intersection(set(arr2)).intersection(set(arr3)))
+        arr.sort()
+        return arr
