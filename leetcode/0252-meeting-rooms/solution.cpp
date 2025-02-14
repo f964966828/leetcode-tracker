5907a96a4930ce7ada6c7d4e06daee0843e82a89
Frank Lin <f964966828@gmail.com>
2025-02-14 12:34:48 +0000

    [LeetCode] - 0252 Easy   - Runtime     0 ms (100.00%), Memory    15.9 MB ( 79.28%) - Meeting Rooms

---
+++ leetcode/0252-meeting-rooms/solution.cpp
@@ -0,0 +1,16 @@
+bool cmp(vector<int>& a, vector<int>& b) {
+    return a[0] < b[0];
+}
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), cmp);
+        int n = intervals.size();
+        
+        for(int i=0; i<n-1; i++){
+            if(intervals[i][1] > intervals[i+1][0]) return false;
+        }
+        return true;
+    }
+};
