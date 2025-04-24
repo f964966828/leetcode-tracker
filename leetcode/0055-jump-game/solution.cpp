05a8da3e8e24355831f5d153597d8e464069fdeb
Frank Lin <f964966828@gmail.com>
2025-04-24 04:54:25 +0000

    [LeetCode] - 0055 Medium - Runtime     0 ms (100.00%), Memory    52.3 MB ( 83.21%) - Jump Game

--- leetcode/0055-jump-game/solution.cpp
+++ leetcode/0055-jump-game/solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-
-        if(nums.size() == 1) 
+        if (nums.size() == 1) 
             return true;
 
         int step = nums[0], idx = 1;
-        while(step){
-            step = max(step-1, nums[idx++]);
-            if(idx == nums.size()) 
+        while (step) {
+            step = max(step - 1, nums[idx++]);
+            if (idx == nums.size()) 
                 return true;
         }
 
