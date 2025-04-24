829782fa65491a28b3e6350db87022251e546de1
Frank Lin <f964966828@gmail.com>
2025-04-24 04:47:21 +0000

    [LeetCode] - 0121 Easy   - Runtime     0 ms (100.00%), Memory    97.2 MB ( 99.49%) - Best Time to Buy and Sell Stock

--- leetcode/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ leetcode/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size(), pmin[n];
-        pmin[0] = 1e9;
-        for(int i=1; i<n; i++) pmin[i] = min(pmin[i-1], prices[i-1]);
-        
-        int ans = 0;
-        for(int i=1; i<n; i++) ans = max(ans, prices[i] - pmin[i]);
+        int mn = INT_MAX, ans = INT_MIN;
+        for (int num : prices) {
+            mn = min(mn, num);
+            ans = max(ans, num - mn);
+        }
         return ans;
     }
 };
