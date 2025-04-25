13f5328f796b9bc592d6a65b6b1cc5eafabacdb7
Frank Lin <f964966828@gmail.com>
2025-04-25 22:01:41 +0000

    [LeetCode] - 0280 Medium - Runtime     0 ms (100.00%), Memory    17.7 MB ( 79.14%) - Wiggle Sort

---
+++ leetcode/0280-wiggle-sort/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (i % 2) { // samller
+                if (nums[i] < nums[i + 1]) swap(nums[i], nums[i + 1]);
+            } else { // larger
+                if (nums[i] > nums[i + 1]) swap(nums[i], nums[i + 1]);
+            }
+        }
+    }
+};
