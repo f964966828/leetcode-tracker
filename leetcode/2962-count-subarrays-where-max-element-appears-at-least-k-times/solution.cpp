7946bda26bcfb9c2caf4e241ff53f574670ee6a9
Frank Lin <f964966828@gmail.com>
2025-04-29 01:32:56 +0000

    [LeetCode] - 2962 Medium - Runtime     0 ms (100.00%), Memory   121.5 MB (100.00%) - Count Subarrays Where Max Element Appears at Least K Times

--- leetcode/2962-count-subarrays-where-max-element-appears-at-least-k-times/solution.cpp
+++ leetcode/2962-count-subarrays-where-max-element-appears-at-least-k-times/solution.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
-        int mx = 0;
-        for (int num: nums) mx = max(mx, num);
-
-        vector<int> p_cnt(1, 0);
-        for (int i = 0; i < nums.size(); i++) {
-            p_cnt.push_back(p_cnt[i] + (nums[i] == mx));
-        }
+        int mx = *max_element(nums.begin(), nums.end());
 
         long long ans = 0;
-        for (int i = 0; i < p_cnt.size(); i++) {
-            auto iter = lower_bound(p_cnt.begin(), p_cnt.end(), p_cnt[i] - k + 1);
-            //cout << p_cnt[i] << " " << *iter << " " << (iter - p_cnt.begin()) << endl;
-            if (p_cnt[i] - *iter + 1 >= k) {
-                ans += (iter - p_cnt.begin());
-            }
+        int sum = 0, l = 0, n = nums.size();
+        for (int r = 0; r < n; r++) {
+            sum += (nums[r] == mx);
+            while (sum >= k) sum -= (nums[l++] == mx);
+            if (sum == k - 1) ans += l;
         }
 
         return ans;
