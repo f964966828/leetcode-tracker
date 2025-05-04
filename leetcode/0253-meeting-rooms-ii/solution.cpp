af7d6ff3a776b15067c010a62b8fd8df1cf1e146
Frank Lin <f964966828@gmail.com>
2025-05-04 13:52:48 +0000

    [LeetCode] - 0253 Medium - Runtime     0 ms (100.00%), Memory    15.9 MB ( 99.80%) - Meeting Rooms II

--- leetcode/0253-meeting-rooms-ii/solution.cpp
+++ leetcode/0253-meeting-rooms-ii/solution.cpp
@@ -5,12 +5,11 @@ bool cmp(vector<int> &a, vector<int> &b) {
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        priority_queue<int, vector<int>, greater<int>> pq;
         sort(intervals.begin(), intervals.end(), cmp);
-        for (auto interval: intervals) {
-            if (pq.size() != 0 && pq.top() <= interval[0]) {
-                pq.pop();
-            }
+        
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (auto &interval: intervals) {
+            if (pq.size() != 0 && pq.top() <= interval[0]) pq.pop();
             pq.push(interval[1]);
         }
 
