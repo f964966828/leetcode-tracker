7847a65bd043f2f88a30d29be2e04dbfcdbe04b6
Frank Lin <f964966828@gmail.com>
2025-05-19 01:40:14 +0000

    [LeetCode] - 3024 Easy   - Runtime     0 ms (100.00%), Memory    22.8 MB ( 92.33%) - Type of Triangle

---
+++ leetcode/3024-type-of-triangle/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int a = nums[0], b = nums[1], c = nums[2];
+        if (a + b <= c) {
+            return "none";    
+        } else if (a == c) {
+            return "equilateral";
+        } else if (a == b || b == c) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+};
