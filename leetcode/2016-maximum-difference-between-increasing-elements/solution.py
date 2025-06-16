06a139a12a663fd30244b35ff487a54bf5f7f333
Frank Lin <f964966828@gmail.com>
2025-06-16 01:57:46 +0000

    [LeetCode] - 2016 Easy   - Runtime     0 ms (100.00%), Memory    17.9 MB (100.00%) - Maximum Difference Between Increasing Elements

---
+++ leetcode/2016-maximum-difference-between-increasing-elements/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        ans = -1
+        mn = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] > mn:
+                ans = max(ans, nums[i] - mn)
+            mn = min(mn, nums[i])
+        return ans
+
