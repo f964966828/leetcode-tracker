efddfaee0b826e6d75b37acafe246e636a103dba
Frank Lin <f964966828@gmail.com>
2025-08-30 05:18:31 +0000

    [LeetCode] - 0036 Medium - Runtime     0 ms (100.00%), Memory    22.7 MB ( 57.13%) - Valid Sudoku

--- leetcode/0036-valid-sudoku/solution.cpp
+++ leetcode/0036-valid-sudoku/solution.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         int rflag[9][9] = {0}, cflag[9][9] = {0}, bflag[9][9] = {0};
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-                if(board[i][j] == '.') continue;
-                int b = 3*(i/3) + j/3, v = board[i][j]-'1';
-                if(rflag[i][v] || cflag[j][v] || bflag[b][v])
-                    return false;
-                else
-                    rflag[i][v] = cflag[j][v] = bflag[b][v] = 1;
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++){
+                if (board[r][c] == '.') continue;
+                int b = 3 * (r / 3) + c / 3;
+                int v = board[r][c] - '1';
+                if (rflag[r][v] || cflag[c][v] || bflag[b][v]) return false;
+                else rflag[r][v] = cflag[c][v] = bflag[b][v] = 1;
             }
         }
         return true;
