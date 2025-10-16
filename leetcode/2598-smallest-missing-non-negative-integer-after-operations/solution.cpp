b4fa232a4a9999175f01f165bb8e6fae2e909f50
Frank Lin <f964966828@gmail.com>
2025-10-16 07:24:11 +0000

    [LeetCode] - 2598 Medium - Runtime   169 ms ( 13.05%), Memory   138.6 MB ( 45.43%) - Smallest Missing Non-negative Integer After Operations

--- leetcode/2598-smallest-missing-non-negative-integer-after-operations/solution.cpp
+++ leetcode/2598-smallest-missing-non-negative-integer-after-operations/solution.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int value) {
-        vector<int> count(value);
-        set<int> s;
-        for(int num: nums){
-            if(num < 0)
-                num -= (num/value-1) * value;
-                
-            int base = num%value;
-            s.insert(base + value*count[base]);
-            count[base]++;       
+        map<int, int> mp;
+        for (int & num : nums) mp[(num % value + value) % value]++;
+        
+        int val = 0;
+        for (int i = 1; i < value; i++) {
+            if (mp[i] < mp[val]) val = i;
         }
         
-        int ans = 0;
-        for(int num: s){
-            if(num == ans)
-                ans++;
-        }
-        
-        return ans;
+        return value * mp[val] + val;
     }
 };
