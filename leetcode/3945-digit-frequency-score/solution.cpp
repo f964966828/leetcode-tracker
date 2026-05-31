84d414137d79a0f3af171d10e20289773512d7c4
Frank Lin <f964966828@gmail.com>
2026-05-31 08:59:29 +0000

    [LeetCode] - 3945 Easy   - Runtime     0 ms (100.00%), Memory     8.7 MB ( 54.46%) - Digit Frequency Score

---
+++ leetcode/3945-digit-frequency-score/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        vector<int> freq(10);
+        while (n) {
+            freq[n % 10]++;
+            n /= 10;
+        }
+        int ans = 0;
+        for (int i = 0; i < 10; i++) ans += i * freq[i];
+        return ans;
+    }
+};
