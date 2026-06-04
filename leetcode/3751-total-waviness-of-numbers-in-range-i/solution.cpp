8eb32f8edf4d099d02dc4f115c0167406d194089
Frank Lin <f964966828@gmail.com>
2026-06-04 06:49:20 +0000

    [LeetCode] - 3751 Medium - Runtime    50 ms ( 45.88%), Memory     9.3 MB ( 72.51%) - Total Waviness of Numbers in Range I

---
+++ leetcode/3751-total-waviness-of-numbers-in-range-i/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int totalWaviness(int num1, int num2) {
+        int ans = 0;
+        for (int i = num1; i <= num2; i++) {
+            string str = to_string(i);
+            for (int j = 1; j < str.size() - 1; j++) {
+                if (str[j] > str[j - 1] && str[j] > str[j + 1]) ans++;
+                if (str[j] < str[j - 1] && str[j] < str[j + 1]) ans++;
+            }
+        }
+        return ans;
+    }
+};
