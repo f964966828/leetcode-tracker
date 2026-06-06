ef61f3d9d16fae20b9570ad223118bb63e2418c4
Frank Lin <f964966828@gmail.com>
2026-06-06 14:28:59 +0000

    [LeetCode] - 2574 Easy   - Runtime     0 ms (100.00%), Memory    15.2 MB ( 41.89%) - Left and Right Sum Differences

--- leetcode/2574-left-and-right-sum-differences/solution.cpp
+++ leetcode/2574-left-and-right-sum-differences/solution.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-    vector<int> leftRigthDifference(vector<int>& nums) {
+    vector<int> leftRightDifference(vector<int>& nums) {
         int n = nums.size();
-        vector<int> leftSum(n), rightSum(n), ans(n);
-        
-        for(int i=1; i<n; i++) leftSum[i] = leftSum[i-1] + nums[i-1];
-        for(int i=n-2; i>=0; i--) rightSum[i] = rightSum[i+1] + nums[i+1];
-        for(int i=0; i<n; i++) ans[i] = abs(leftSum[i] - rightSum[i]);
-        
+        vector<int> left(n), right(n), ans(n);
+        for (int i = 1; i < n; i++) left[i] = left[i - 1] + nums[i - 1];
+        for (int i = n - 2; i >= 0; i--) right[i] = right[i + 1] + nums[i + 1];
+        for (int i = 0; i < n; i++) ans[i] = abs(left[i] - right[i]);
         return ans;
     }
 };
